71d94611295b6b9316c482da967c987a6bc63769
Debotos Das <debotosdas@gmail.com>
2019-07-17 16:09:55 +0600

    updated isAdmin middleware logic

--- src/GraphQL/resolvers/middleware/authorization.ts
+++ src/GraphQL/resolvers/middleware/authorization.ts
@@ -19,8 +19,33 @@ import { combineResolvers, skip } from 'graphql-resolvers'
 export const isAuthenticated = (_: any, __: any, { me }: any) =>
 	me ? skip : new AuthenticationError('Not authenticated as user.')
 
-export const isAdmin = combineResolvers(isAuthenticated, (_: any, __: any, { me: { role } }) =>
-	role === 'ADMIN' ? skip : new ForbiddenError('Not authorized as admin.')
+export const isAdmin = combineResolvers(
+	isAuthenticated,
+	async (_: any, __: any, { me: { id, role }, models }) => {
+		/* 
+        This step (role === 'ADMIN' at very first) 
+        may not required but this will make the calculation fast.
+        Because if this fails then an additional DB query will not execute
+      */
+		if (role === 'ADMIN') {
+			const user = await models.User.findByPk(id)
+			if (user && user.role === 'ADMIN') {
+				return skip
+			} else {
+				/*
+            In this case, user browser has the token that is sent to the server.
+            Server treated it as a valid token because it contains valid signature.
+            But the problem is in the back some other admin deleted this account.
+            So we are checking that although this user have valid token but in the DB he exist or not. 
+          */
+				return new AuthenticationError(
+					'Your are no longer authorized to perform any admin actions. Your admin account has been deleted.'
+				)
+			}
+		} else {
+			return new ForbiddenError('Not authorized as admin.')
+		}
+	}
 )
 
 export const isMessageOwner = async (_: any, { id }: any, { models, me }: any) => {
